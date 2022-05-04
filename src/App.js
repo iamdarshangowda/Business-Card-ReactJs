78311a9ac29665654f2279b7a2fd07ed4e66cdaf
Darshan Gowda <iamdarshangowda@gmail.com>
2022-05-04 21:33:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import profile from "./profile.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src={profile} alt="profile-photo" />
+        <h1>Darshan Gowda</h1>
+        <p>Frontend Developer</p>
+        <a href="mailto:iamdarshangowda@outlook.com">
+          iamdarshangowda@outlook.com
         </a>
+        <div className="social">
+          <buttons className="git">
+            <i class="fa-brands fa-github"></i>
+          </buttons>
+          <button className="linked">
+            <i class="fa-brands fa-linkedin"></i>
+          </button>
+        </div>
       </header>
+
+      {/* Main section*/}
+      <main className="main">
+        <div className="about">
+          <h4>About</h4>
+          <p>
+            I am a frontend developer with a particular interest in making
+            things simple and automating daily tasks. I try to keep up with
+            security and best practices, and am always looking for new things to
+            learn.
+          </p>
+        </div>
+        <div className="skills">
+          <h4>Skills</h4>
+          <p>HTML, CSS, JavaScript and ReactJs</p>
+        </div>
+      </main>
+      <footer>
+        <a href="https://twitter.com/iamdarshangowda">
+          <i class="fa-brands fa-twitter-square"></i>
+        </a>
+        <a href="https://www.instagram.com/iamdarshangowda/?hl=en">
+          <i class="fa-brands fa-instagram-square"></i>
+        </a>
+      </footer>
     </div>
   );
 }
